a0f9d7a6719ac3121dbd77a7a552cd33630f3bc5
Andrew Bateman <gomezbateman@yahoo.com>
2018-11-21 13:02:07 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-webcam-video-capture';
+export class AppComponent implements OnInit {
+
+    @ViewChild("video") //variable from html
+    public video: ElementRef;
+
+    @ViewChild("canvas") //variable from html
+    public canvas: ElementRef;
+
+    public captures: Array<any>;
+
+    public constructor() {
+        this.captures = [];
+    }
+
+    public ngOnInit() { }
+
+    // view DOM elements. Get user permission first
+    public ngAfterViewInit() {
+        if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+                this.video.nativeElement.src = window.URL.createObjectURL(stream);
+                this.video.nativeElement.play();
+            });
+        }
+    }
+    //capture an image and add it to the captures array. 
+    public capture() {
+        var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
+        this.captures.push(this.canvas.nativeElement.toDataURL("image/png"));
+    }
+
 }
